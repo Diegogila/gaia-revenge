16e59c3090bb496c882f1d9516bd017cd0d50a47
diegogil <dgg26496@hotmail.com>
2023-06-23 14:55:10 -0600

    Se termina movimientos basicos de player y parte de la funcionalidad

--- src/components/player.js
+++ src/components/player.js
@@ -2,7 +2,38 @@ class Player{
     constructor(){
         this.y;
         this.x;
-        this.emoji = '🤠'
+    }
+
+    move(map, eventKey){
+        const direction ={x:0, y:0};
+        let colisionObj;
+        switch (eventKey) {
+            case "w":
+                direction.y = -1;
+                break;
+            case "s":
+                direction.y = 1;
+                break;
+            case "d":
+                direction.x = 1;
+                break;
+            case "a":
+                direction.x = -1;
+                break;                                            
+            default:
+                break;
+        }
+        colisionObj = map.mapCols[this.y+direction.y][this.x+direction.x];
+
+        if (colisionObj != 'X') {
+            console.log(colisionObj);
+            
+            map.mapCols[this.y][this.x] = colisionObj;
+            this.y = this.y + direction.y;
+            this.x = this.x + direction.x;
+            map.mapCols[this.y][this.x] = 'I';
+        }
+
     }
 }
 
--- src/game.js
+++ src/game.js
@@ -7,8 +7,35 @@ const game = canva.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame(){
-    const canvasSice
+    let canvasSize;
+    if (window.innerWidth > window.innerHeight) {
+        canvasSize = window.innerWidth * 0.5;
+    } else {
+        canvasSize = window.innerWidth * 0.75;
+    }
+
+
+    canva.setAttribute('width',canvasSize);
+    canva.setAttribute('height', canvasSize);
+    
+    const elementSize = canvasSize/36;
+    game.font = `${elementSize}px Verdana`;
+    game.textAlign = 'end';
+
+
     const player = new Player();
-    const map = new Map(player);
+    const map = new Map(player, elementSize);
+    
+    map.mapBuild();
+    map.render(game);
+
+
+    window.addEventListener("keydown", function(event) {
+        game.clearRect(0,0,canvasSize,canvasSize);
+        player.move(map,event.key);
+        map.render(game);
+      });
+
+
 }
 
